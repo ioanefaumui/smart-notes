8b67cf82d73c048b94eaf4c47e6949a232457876
ioaneSuflex <ioane@suflex.com.br>
2024-07-19 15:46:19 -0300

    chore: config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: () => ({
+        white: "#F7F7F7",
+        accent: "#EFECE6",
+        gray: {
+          light: "#BABABA",
+          dark: "#737373",
+        },
+        jetblack: "#080808",
+        teal: {
+          light: "#048C80",
+          dark: "#017373",
+        },
+      }),
     },
   },
   plugins: [],
